e0a115547c1a5a8b3ca17dd319c732865df8b57d
gustavofaria <gsilva.00@hotmail.com>
2020-08-19 16:07:20 -0300

    Modularização e uso de média

--- activity 2/preencher.py
+++ activity 2/preencher.py
@@ -1,29 +1,61 @@
 import csv
 import numpy as np
-print("Por favor digite o nome do CSV a ser testado")
-nome=input()
-#nome='crx.data'
+
+def calculate_avg(coluna):
+	soma = 0
+	cont = 0
+	for i in range(0,len(data[0])):
+		if (data[coluna][i]!="?"):
+			soma+=data[coluna][i]
+			cont+=1
+
+	return soma/cont
+
+def calculate_moda(coluna):
+	moda={}
+	max=-1
+	for j in range(0,len(data)):
+		if (data[j][coluna]=="?"):
+			continue
+		elif (data[j][coluna] in moda):
+			moda[data[j][coluna]]=moda[data[j][i]]+1
+		else:
+			moda[data[j][coluna]]=1	
+		if (moda[data[j][coluna]]>max):
+			max=moda[data[j][coluna]]
+			elem=data[j][coluna]
+	return elem
+
+def substitute(coluna, valor):
+	print("Substituindo a coluna ", coluna, " com o valor ", valor)
+	for j in range (0,len(data)):
+		if (data[j][coluna] == "?"):
+			data[j][coluna] = valor
+
+#print("Por favor digite o nome do CSV a ser testado")
+#nome=input()
+nome='crx.csv'
 with open(nome, newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
+
 for i in range(0,len(data[0])):
-	moda={}
-	falta=0
-	max=-1
+	temFaltante = False
+	eMedia = False
 	for j in range (0,len(data)):
-		if(data[j][i]=="?"):
-			flag=1
-			continue
-		elif (data[j][i] in moda):
-			moda[data[j][i]]=moda[data[j][i]]+1
-		else:
-			moda[data[j][i]]=1	
-		if(moda[data[j][i]]>max):
-			max=moda[data[j][i]]
-			elem=data[j][i]
-	if(flag==1):
-		for j in range (0,len(data)):
-			if(data[j][i]=="?"):
-				data[j][i]=elem
+		if (data[j][i]=="?"):
+			temFaltante = True
+		if (isinstance(data[j][i],float)):
+			eMedia = True
+		if (temFaltante and eMedia):
+			break
+	
+	if (temFaltante):
+		if (eMedia):
+			substituinte = calculate_avg(i)
+		else :
+		  substituinte = calculate_moda(i)  
+		substitute(i, substituinte)
+	
 
 np.savetxt("resultado.csv", data, delimiter=",", fmt='%s')
